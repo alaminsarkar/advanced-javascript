de3b1605353e63494a8f65368d433ab5d1852116
alaminsarkar <alaminsarkar899@gmail.com>
2021-02-04 10:32:49 +0600

    double vs treepole equal

---
+++ duble vs treepol equal.js
@@ -0,0 +1,19 @@
+// const first =2;
+// const second =2;
+// if(first==second)
+// {
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+
+// const num1= 1;
+// const num2 ='2';
+// if(num1===num2){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
